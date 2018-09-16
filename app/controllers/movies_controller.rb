a6879c4d162df43ded877156494aec092b4ae22d
Maria Dees <mcdees2010@gmail.com>
2018-09-16 15:32:40 -0700

    added the create method to add a new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,19 @@ class MoviesController < ApplicationController
     def new
         @movie = Movie.new
     end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+            redirect_to movie_path(@movie)
+        else
+            redirect_to 'new'
+        end
+    end
+
+    private 
+        def movie_params 
+            params.require(:movie).permit(:title, :description, :director, :runtime, :year, :rating, :image)
+        end
 end
