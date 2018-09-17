e18acdfa0812e13b5ef9c3adcaed96181263b2c3
Maria Dees <mcdees2010@gmail.com>
2018-09-16 20:52:40 -0700

    added edit and delete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,28 @@ class MoviesController < ApplicationController
         end
     end
 
+    def edit
+        @movie = Movie.find params[:id]
+    end
+
+    def update
+        @movie = Movie.find params[:id]
+        
+        if @movie.update(movie_params)
+            redirect_to movie_path(@movie)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @movie = Movie.find params[:id]
+
+        @movie.destroy
+        redirect_to root_path
+        
+    end
+
     private 
         def movie_params 
             params.require(:movie).permit(:title, :description, :director, :runtime, :year, :rating, :image)
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  has_secure_password
+    # Verify that an email exists and that it does not already exist in the db
+  validates :email, presence: true, uniqueness: true
 end
