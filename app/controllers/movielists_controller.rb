85264992c816a2a17edee8b99c73a8cb11c53bb2
Maria Dees <mcdees2010@gmail.com>
2018-09-19 16:48:27 -0700

    repositioned my login and signup pages

--- app/controllers/movielists_controller.rb
+++ app/controllers/movielists_controller.rb
@@ -3,7 +3,7 @@ class MovielistsController < ApplicationController
     def index
         if params[:search] #if a user has searched for a movie
             sanitized = params[:search].split.map { |i| i.capitalize }.join(' ') #turn what the user searched in capitalized words by each element in an array
-            @movies = Movie.where("title LIKE ? ", "%#{sanitized}%") #queries movies searched by the sanitized parameter, finding a similar title
+            @movies = Movie.where("title LIKE ?", "%#{sanitized}%") #queries movies searched by the sanitized parameter, finding a similar title
         else
             @movies = Movie.all
         end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,14 +38,7 @@ class MoviesController < ApplicationController
         end
     end
 
-    def search
-    
-        # if params[:search].blank?  
-        #     redirect_to(root_path, alert: "Empty field!") and return  
-        #   else  
-        #     @parameter = params[:search].downcase  
-        #     @results = Store.all.where("lower(name) LIKE :search", search: @parameter) 
-        # end  
+    def search 
     end
 
     def destroy
