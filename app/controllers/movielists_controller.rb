d382d644973d407645d5eb180a145930aa5d8b1f
Maria Dees <mcdees2010@gmail.com>
2018-09-19 16:19:30 -0700

    search bar title

--- app/controllers/movielists_controller.rb
+++ app/controllers/movielists_controller.rb
@@ -4,7 +4,6 @@ class MovielistsController < ApplicationController
         if params[:search] #if a user has searched for a movie
             sanitized = params[:search].split.map { |i| i.capitalize }.join(' ') #turn what the user searched in capitalized words by each element in an array
             @movies = Movie.where("title LIKE ? ", "%#{sanitized}%") #queries movies searched by the sanitized parameter, finding a similar title
-
         else
             @movies = Movie.all
         end
